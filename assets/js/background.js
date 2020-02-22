f70b785ea6a25a4ba23fdfa9d2244156f03c1e4d
Dawid Klatkiewicz <klatkiewiczdawid@gmail.com>
2020-02-21 20:40:31 -0500

    redid bg
    Signed-off-by: Dawid Klatkiewicz <klatkiewiczdawid@gmail.com>

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,201 +1,144 @@
-class Rock {
 
-   constructor(x, y, r, vx, vy, cd) {
-      this.x = x || 0;
-      this.y = y || 0;
-      this.cd = cd || 0;
+class bgObject {
 
-      this.vx = vx || random(-1,1);
-      this.vy = vy || random(-1,1);
-
-      this.r = r || random(60,120);
-
-      this.angle = 0;
-      this.angleMod = (random(PI / 250, PI / 295)) *  (random() > 0.5 ? -1 : 1);
-
-      const radMod = log(this.r/2) * 2.75;
-
-      this.radii = [];
-      
-      for (let i = 0; i < 20; i++) {
-         const r = random((this.r) - radMod, (this.r) + radMod);
-
-         this.radii.push(r);
-      }
-
+   constructor(x) {
+      this.x = x || width / 2;
+      this.w = random(30,50);
+      this.y = -height - this.w;
+      this.vy = random(5,10);
+      this.a = 0;
+      this.da = random(PI/200,PI/120) * (random() < 0.5 ? 1 : -1);
+      this.c = {r:random(100,255),g:random(100,255),b:random(100,255)};
    }
 
-   draw() {
-
-      if (this.cd > 0) this.cd -= 1;
-
-      this.x += this.vx;
+   draw() { 
       this.y += this.vy;
+      this.a += this.da;
 
-      if (this.x + (this.r / 2) > width) {
-         this.x -= (this.x + (this.r / 2)) - width;
-         this.vx *= -1;
-      }
-
-      if (this.y + (this.r / 2) > height) {
-         this.y -= (this.y + (this.r / 2)) - height;
-         this.vy *= -1;
-      }
-
-      if (this.x - (this.r / 2) < 0) {
-         this.x += (this.x - (this.r / 2)) * -1; 
-         this.vx *= -1;
-      }
-
-      if (this.y - (this.r / 2) < 0) {
-         this.y += (this.y - (this.r / 2)) * -1; 
-         this.vy *= -1;
-      }
-
-      rocks.forEach(rock => {
-
-         if (this == rock) return;
-
-         if (this.isColliding(rock) && this.cd == 0) {
-            this.vx *= -1;
-            this.vy *= -1;
-            this.cd = 5;
-         }
-      });
-
-      stroke(220);
+      push()
+      fill(255,0,0);
+      //rect(this.x - (this.w),this.y - (this.w),this.w*2,this.w*2);
+      //fill(0,255,0);
+      //rect(this.x - (this.w/2),this.y - (this.w/2),this.w,this.w);
+      normalMaterial(255);
       noFill();
-      //ellipse(this.x,this.y,this.r);
-
-      push();
-      translate(this.x,this.y)
-      rotate(this.angle);
-
-      beginShape();
-
-      let currR = 0;
-
-      for(let a = 0; a < TWO_PI; a += TWO_PI/25) {
-
-         const nr = this.radii[currR];
-
-         const x = ((nr/2) * cos(a));
-         const y = ((nr/2) * sin(a));
-
-         vertex(x,y);
-
-         currR = currR < this.radii.length ? currR + 1 : 0;
-      }
-
-      endShape(CLOSE);
-
+      stroke(this.c.r,this.c.g,this.c.b);
+      translate(this.x,this.y);
+      //rotateX(this.a);
+      //rotateY(this.a);
+      rotateZ(this.a);
+      box(this.w);
       pop();
-
-      this.angle += this.angleMod;
    }
 
-   split() {
-      if (this.r > 30) {
-         const r1 = new Rock(this.x,this.y - this.r/4,this.r/2,this.vx * 1.1,this.vy * 1.1, 60);
-         const r2 = new Rock(this.x - this.r/4,this.y,this.r/2,-this.vx * 1.1,this.vy * 1.1, 60);
-         const r3 = new Rock(this.x,this.y + this.r/4,this.r/2,this.vx * 1.1 ,-this.vy * 1.1, 60);
-
-         rocks.push(r1,r2,r3);
-      }
-      rocks.splice(rocks.indexOf(this), 1);
-   }
-
-   isColliding(rock) {
-      const d = {x: abs(rock.x - this.x), y: abs(rock.y - this.y)};
-      const dist = sqrt((d.x * d.x) + (d.y * d.y));
-      return dist < (rock.r / 2) + (this.r / 2);
-   }
-
-   nonSpawnable() {
-      if (rocks.length == 0) return false;
-      for (let rock of rocks) {
-         if (this.isColliding(rock)) {
-            return rock;
-         }
-      }
-      return false;
+   isOutside() {
+      return (this.y > height);
    }
 
 }
 
 class Particle {
-
-   constructor(x,y,vx,vy,ax,ay) {
-
-      this.pos = {x, y};
-      this.v = {x:vx, y:vy};
-      this.a = {x:ax, y:ay};
-      this.size = random(2,5);
-      this.color = 255;
-
+   constructor(x,y,c) {
+      this.x = x;
+      this.y = y;
+      this.z = 0;
+      this.vx = random(-2,2);
+      this.vy = random(-2,2);
+      this.ax = 0;
+      this.ay = 0.25;
+      this.c = c || {r:random(100,255),g:random(100,255),b:random(100,255)};
+      this.a = 0;
+      this.da = random(PI/200,PI/120) * (random() < 0.5 ? 1 : -1);
+      this.w = random(5,15);
    }
 
    draw() {
-      this.a.y += 0.0075;
+      this.vx += this.ax;
+      this.vy += this.ay;
+      this.x += this.vx;
+      this.y += this.vy;
+      this.a += this.da;
 
-      this.v.x += this.a.x;
-      this.v.y += this.a.y;
+      push();
+      translate(this.x,this.y);
+      normalMaterial(255);
+      noFill();
+      stroke(this.c.r,this.c.g,this.c.b);
+      rotateX(this.a);
+      rotateY(this.a);
+      rotateZ(this.a);
+      box(this.w);
+      pop();
+   }
 
-      this.pos.x += this.v.x;
-      this.pos.y += this.v.y;
-
-      fill(this.color);
-      stroke(this.color);
-      this.color -= 3;
-      ellipse(this.pos.x,this.pos.y,this.size);
+   isOutside() {
+      return (this.y > (height / 2) + 20);
    }
 }
 
-function mousePressed() {
-   const m = new Rock(mouseX,mouseY,2);
-   const r = m.nonSpawnable();
-   if (r) {
-      for (let i = 0; i < random(40,80); i++)
-         particles.push(new Particle(mouseX,mouseY,0,0,random(-.14,.14),random(-.14,.14)));
-         r.split();
-   }
-}
-
-const rocks = [];
+const shapes = [];
 const particles = [];
-const ROCK_AMOUNT = 50;
+let timer;
 
 function setup() {
-   createCanvas(window.innerWidth, window.innerHeight);
+   createCanvas(window.innerWidth, window.innerHeight, WEBGL);
 
-   for (let i = 0; i < ROCK_AMOUNT; i++) {
-      let newRock = new Rock(random(width),random(height));
-
-      while (newRock.nonSpawnable()) {
-         newRock = new Rock(random(width),random(height));
+   timer = setInterval(() => {
+      let ns = new bgObject(random(-width/2,width/2));
+      let tries = 0;
+      while(!isSpawnable(ns)) {
+         if (tries > 10) return;
+         ns = new bgObject(random(-width/2,width/2));
+         tries += 1;
       }
-
-      rocks.push(newRock);
-   }
+      shapes.push(ns);
+   }, 125);
 }
+
 
 function draw() {
    background(0);
 
-   fill(255);
-   noStroke();
-   text("Particles: " + particles.length, 20, 20);
-   text("Entities: " + (particles.length + rocks.length), 20, 40);
-   text(`FPS: ${frameRate().toFixed(0)}`, 20, 60);
-   rocks.forEach(r => r.draw());
-
-   for(let i = 0; i < particles.length; i++) {
-      if (particles[i].pos.x > width || particles[i].pos.x < 0 ||
-         particles[i].pos.y > height || particles[i].pos.y < 0) {
-         particles.splice(i, 1);
-         continue;
+   for(let i = shapes.length - 1; i > -1; i--) {
+      shapes[i].draw();
+      if (shapes[i].isOutside()) {
+         shapes.splice(i, 1);
       }
+   }
+   for (let i = particles.length - 1; i > -1; i--) {
       particles[i].draw();
+      if (particles[i].isOutside()) {
+         particles.splice(i, 1);
+      }
    }
 }
 
+function mousePressed() {
+   for (let i = shapes.length - 1; i > -1; i--) {
+      if (isOver(shapes[i])) {
+         for(let i = 0; i < random(10,30); i++)
+            particles.push(new Particle(mouseX-(width/2), mouseY-(height/2), shapes[i].c));
+         shapes.splice(i,1);
+         break;
+      }
+   }
+} 
+function isOver(shape) {
+   return (mouseX - (width/2) > shape.x - shape.w &&
+           mouseX - (width/2) < shape.x + shape.w &&
+           mouseY - (height/2) > shape.y - shape.w &&
+           mouseY - (height/2) < shape.y + shape.w); 
+}
+
+function isSpawnable(s) {
+   for (c of shapes) {
+      //      x                 y                w
+      //rect(this.x - (this.w),this.y - (this.w),this.w*2);
+      // rect1x < rect2x+rect2w && rect1x + rect1w > rect2x   
+      if (s.x < c.x + c.w && s.x + s.w > c.x) { 
+      //if(s.x - s.w < (c.x - c.w) + (c.w * 2) && (s.x - s.w) + (c.w * 2) > c.w - c.w){
+         return false;
+      }
+   }
+   return true;
+}
